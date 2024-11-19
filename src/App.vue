85373c0f7f797a125f6a68832f2923732994935e
Eldar Sayranov <sairanoff.05@yandex.ru>
2024-11-19 20:45:02 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>Крестики-Нолики</h1>
+    <GameBoard />
+  </div>
+</template>
+
+<script>
+import GameBoard from './components/GameBoard.vue';
+
+export default {
+  name: 'App',
+  components: {
+    GameBoard
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: #D4DBE2;
+}
+
+h1 {
+  font-family: "Martian Mono", sans-serif;
+  font-size: 1.8rem;
+  color: #7B586B;
+}
+
+#app {
+  text-align: center;
+  font-family: 'Arial', sans-serif;
+}
+</style>
